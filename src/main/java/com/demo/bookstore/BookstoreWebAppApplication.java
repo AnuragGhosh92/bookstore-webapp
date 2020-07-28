f92b813ec1731c099d94147e354d936e917e28b6
Anurag Ghosh <anurag.ghosh92@gmail.com>
2020-07-28 22:06:24 +0530

    Initial Commit
    Initial Commit of Spring Boot App

---
+++ src/main/java/com/demo/bookstore/BookstoreWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.demo.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreWebAppApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreWebAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/demo/bookstore/controllers/HomePageController.java
@@ -0,0 +1,14 @@
+package com.demo.bookstore.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController
+{
+	@RequestMapping(value = "/home")
+	public String home() {
+		System.out.println("Hi");
+		return "home.jsp";
+	}
+}
